963128282390e5d4b283c6fa691424da8cf53056
RanjanMishra92 <mishraranjan92@gmail.com>
2018-08-07 02:03:21 +0530

    Add project files.

---
+++ CaptureScreen/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace CaptureScreen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CallMethod();
+        }
+
+        public static void CallMethod()
+        {
+            Rectangle bounds = Screen.GetBounds(Point.Empty);
+            using (Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height))
+            {
+                using (Graphics g = Graphics.FromImage(bitmap))
+                {
+                    g.CopyFromScreen(new Point(bounds.Left, bounds.Top), Point.Empty, bounds.Size);
+                }
+                bitmap.Save("C:\\temp\\test.jpg", ImageFormat.Jpeg);
+            }
+        }
+
+
+    }
+}
---
+++ ScreenCaptureService/Service1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace ScreenCaptureService
+{
+    public partial class Service1 : ServiceBase
+    {
+        Timer timer = new Timer();
+
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            CaptureScreen();
+            timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
+            timer.Interval = 30000;  //Read from configuration.
+        }
+
+        private void OnElapsedTime(object source, ElapsedEventArgs e)
+        {
+            CaptureScreen();
+        }
+
+        protected override void OnStop()
+        {
+            // log service status
+        }
+
+        public void CaptureScreen()
+        {
+            // Write screen capture logic
+
+        }
+    }
+
+    // InstallUtill.exe path of service and enter from cmd to install the service.
+}
